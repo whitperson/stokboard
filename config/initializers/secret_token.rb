75c1fdc26dd38eb216661742ccc995dde0d4b927
whitperson <whitperson@gmail.com>
2012-12-07 10:11:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stokboard::Application.config.secret_token = '372805aeba5ba62fcc28b795c05c625b17c87eafd6d897bfa8c3102be3a382191e85b6d5d43ed898e218ff362be1213548ccd99c4b1906718a26348ee006edef'
